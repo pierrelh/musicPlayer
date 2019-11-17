4eddb6d8d48adb7c37c8daa5338207b2355f5ac6
pierrelh <lhopital.pierre@gmail.com>
2019-11-17 20:00:45 +0100

    work on music player

--- assets/reader.php
+++ assets/reader.php
@@ -1,21 +1,18 @@
 <link rel="stylesheet" href="<?php echo $link ?>/styles/readerStyle.css">
-<section id="reader">
-    <div id="audio-player">
-      <ul>
-        <li>
-          <p id="songName"></p>
-        </li>
-        <li>
-          <div id="controls">
-            <i id="play" class="fa fa-pause"></i>
-            <span id="start" class="time">00:00</span>
-            <div id="progressbar"></div>
-            <span id="time" class="time">00:00</span>
-            <i id="mute" class="fa fa-volume-up"></i>
-            <div id="volume"></div>
-          </div>
-        </li>
-      </ul>
-
-    </div>
+<section id="audio-player">
+  <ul>
+    <li>
+      <p id="songName"></p>
+    </li>
+    <li>
+      <div id="controls">
+        <i id="play" class="fa fa-pause"></i>
+        <span id="start" class="time">00:00</span>
+        <div id="progressbar"></div>
+        <span id="time" class="time">00:00</span>
+        <i id="mute" class="fa fa-volume-up"></i>
+        <div id="volume"></div>
+      </div>
+    </li>
+  </ul>
 </section>
