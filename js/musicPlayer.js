707814ac9e06c6e9bc6c86aa289db1baf113d544
Pierre L'Hopital <lhopital.pierre@gmail.com>
2019-12-10 23:19:22 +0100

    rework progress bar style

--- js/musicPlayer.js
+++ js/musicPlayer.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
   var audio_player = $("#audio-player");
   var start = $("#start");
   var play_button = $('#play');
-  // var progress_bar = $("#progressbar");
   var time = $("#time");
   var mute_button = $('#mute');
   var progressBar = document.getElementById('progress-bar');
@@ -73,50 +72,11 @@ $(document).ready(function () {
         ')');
 });
 
-// $("#progress-bar").mousemove(function (e) {
-//   var val = ($(this).val() - $(this).attr('min')) / ($(this).attr('max') - $(this).attr('min'));
-//   var percent = val * 100;
-//
-//   $(this).css('background-image',
-//       '-webkit-gradient(linear, left top, right top, ' +
-//       'color-stop(' + percent + '%, #FFF), ' +
-//       'color-stop(' + percent + '%, #0B0B0B)' +
-//       ')');
-// });
-
   function getTime(t) {
     var m = ~~(t / 60),
       s = ~~(t % 60);
     return (m < 10 ? "0" + m : m) + ':' + (s < 10 ? "0" + s : s);
   }
-
-  // function getProgressBarClickInfo(progress_bar, e) {
-  //   var offset = progress_bar.position();
-  //   var x = e.pageX - offset.left;
-  //   var y = e.pageY - offset.top;
-  //   var max = progress_bar.progressbar("option", "max");
-  //   var value = x * max / progress_bar.width();
-  //
-  //   return {
-  //     x: x,
-  //     y: y,
-  //     max: max,
-  //     value: value
-  //   };
-  // }
-
-  // progressBar.slider({
-  //   value: player.currentTime,
-  // });
-
-  // progress_bar.progressbar({
-  //   value: player.currentTime,
-  // });
-
-  // progress_bar.click(function(e) {
-  //   var info = getProgressBarClickInfo($(this), e);
-  //   player.currentTime = player.duration / info.max * info.value;
-  // });
 
   progressBar.addEventListener('input', function(){
     player.currentTime = player.duration / progressBar.max * progressBar.value;
