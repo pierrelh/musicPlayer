84f854e97566ecb8ee28aef8c4d18a0ce8a3d9e0
Pierre L'Hopital <lhopital.pierre@gmail.com>
2019-12-10 22:10:11 +0100

    fix play/pause button

--- js/page-scripts.js
+++ js/page-scripts.js
@@ -9,7 +9,9 @@ function mediaPlayerAppear(link, cover, artist, name){
   document.getElementById('cover').src = cover;
   var audioPlayer = document.getElementById('audio-player');
   audioPlayer.className = '';
+  
   var play_button = $('#play');
+  var player = document.getElementById('musicPlayer');
   $(play_button).toggleClass("fa-play", !player.paused);
   $(play_button).toggleClass("fa-pause", player.paused);
 }
