a5617ff491f9f3875a51a87d8ac2502b803abebe
Pierre L'Hopital <lhopital.pierre@gmail.com>
2019-12-10 21:35:37 +0100

    rework volume slider

--- js/musicPlayer.js
+++ js/musicPlayer.js
@@ -37,6 +37,20 @@ $(document).ready(function () {
     $(mute_button).toggleClass("fa-volume-off", player.volume == 0);
   }, false);
 
+  $("#Volume").mousemove(function (e) {
+    var val = ($(this).val() - $(this).attr('min')) / ($(this).attr('max') - $(this).attr('min'));
+    var percent = val * 100;
+
+    $(this).css('background-image',
+        '-webkit-gradient(linear, left top, right top, ' +
+        'color-stop(' + percent + '%, #df7164), ' +
+        'color-stop(' + percent + '%, #F5D0CC)' +
+        ')');
+
+    $(this).css('background-image',
+        '-moz-linear-gradient(left center, #DF7164 0%, #DF7164 ' + percent + '%, #F5D0CC ' + percent + '%, #F5D0CC 100%)');
+});
+
   function getTime(t) {
     var m = ~~(t / 60),
       s = ~~(t % 60);
