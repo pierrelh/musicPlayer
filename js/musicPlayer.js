0a2fd92c1782982666b5e393197246cadb78ddba
Pierre L'Hopital <lhopital.pierre@gmail.com>
2019-12-10 20:00:57 +0100

    rework volume slider

--- js/musicPlayer.js
+++ js/musicPlayer.js
@@ -34,9 +34,9 @@ $(document).ready(function () {
   volumeSlider.addEventListener('input', function(){
     player.volume = volumeSlider.value / 100;
     if (player.volume == 0) {
-      mute_button.toggleClass('fa-volume-off');
+      mute_button.className = 'fa fa-volume-off';
     }else if (mute_button.className == 'fa fa-volume-off') {
-      mute_button.toggleClass('fa-volume-on');
+      mute_button.className = 'fa fa-volume-on';
     }
   }, false);
 
