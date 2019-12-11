bd1df5a49ac26caa584e0397a0409af8d50c8b22
Pierre L'Hopital <lhopital.pierre@gmail.com>
2019-12-11 14:05:47 +0100

    rework media player

--- assets/mediaPlayer.php
+++ assets/mediaPlayer.php
@@ -1,7 +1,5 @@
 <link rel="stylesheet" href="<?php echo $link ?>/styles/mediaPlayerStyle.css">
 <section id="mediaPlayer">
-  <img class="cross" onclick="mediaPlayerHide()" src="../img/white-cross.png" alt="">
-  <img id="cover">
     <audio autoplay controls id="musicPlayer">
     </audio>
 </section>
--- js/page-scripts.js
+++ js/page-scripts.js
@@ -1,19 +1,9 @@
-function mediaPlayerAppear(link, cover, artist, name){
-  // var mediaPlayer = document.getElementById('mediaPlayer');
+function mediaPlayerAppear(link, artist, name){
   var musicPlayer = document.getElementById('musicPlayer');
   var readerPlayer = document.getElementById('readerPlayer');
   var nameTxt = document.getElementById('songName');
   nameTxt.innerHTML = artist + " - " + name;
-  // mediaPlayer.className = 'background-appear';
   musicPlayer.src = link;
-  // document.getElementById('cover').src = cover;
-  var audioPlayer = document.getElementById('audio-player');
-  audioPlayer.className = 'show';
-}
-
-function mediaPlayerHide(){
-  var mediaPlayer = document.getElementById('mediaPlayer');
-  mediaPlayer.className = '';
   var audioPlayer = document.getElementById('audio-player');
   audioPlayer.className = 'show';
 }
