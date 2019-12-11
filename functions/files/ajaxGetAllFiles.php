69529396ac64f8a93c95dc89383c8b49382cb8d1
Pierre L'Hopital <lhopital.pierre@gmail.com>
2019-12-11 12:49:42 +0100

    add filters to library

--- functions/files/ajaxGetAllFiles.php
+++ functions/files/ajaxGetAllFiles.php
@@ -2,7 +2,7 @@
 
   include_once($_SERVER['DOCUMENT_ROOT']."/functions/connexion.php");
   $db = connect();
-  $row = $_POST['row']
+  $row = $_POST['row'];
   $result = pg_query($db, "SELECT * FROM files ORDER BY ".$row." DESC");
   if (!empty($result)) {
     $val = pg_fetch_all($result);
