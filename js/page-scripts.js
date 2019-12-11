e2152a910eb3da0e3955cd1f06449bd526649279
Pierre L'Hopital <lhopital.pierre@gmail.com>
2019-12-11 14:02:23 +0100

    rework song play

--- js/page-scripts.js
+++ js/page-scripts.js
@@ -1,14 +1,14 @@
 function mediaPlayerAppear(link, cover, artist, name){
-  var mediaPlayer = document.getElementById('mediaPlayer');
+  // var mediaPlayer = document.getElementById('mediaPlayer');
   var musicPlayer = document.getElementById('musicPlayer');
   var readerPlayer = document.getElementById('readerPlayer');
   var nameTxt = document.getElementById('songName');
   nameTxt.innerHTML = artist + " - " + name;
-  mediaPlayer.className = 'background-appear';
+  // mediaPlayer.className = 'background-appear';
   musicPlayer.src = link;
-  document.getElementById('cover').src = cover;
+  // document.getElementById('cover').src = cover;
   var audioPlayer = document.getElementById('audio-player');
-  audioPlayer.className = '';
+  audioPlayer.className = 'show';
 }
 
 function mediaPlayerHide(){
