b8b9bae2f34068fbf5f779ef232f90a28a9c38cd
Pierre L'Hopital <lhopital.pierre@gmail.com>
2019-12-12 16:43:14 +0100

    prepare for applying next and previous song

--- js/page-scripts.js
+++ js/page-scripts.js
@@ -1,25 +1,32 @@
 function mediaPlayerAppear(identifier){
-  console.log('ID: ' + identifier);
-  var song = document.getElementById('identifer');
+  var song = document.getElementById('identifier');
   var url = song.dataset.url;
-  var artist = song.dataset.artist;
-  var title = song.dataset.title;
+  var author = song.dataset.artist;
+  var name = song.dataset.title;
   console.log(url);
   console.log(artist);
   console.log(title);
-  // var musicPlayer = document.getElementById('musicPlayer');
-  // var readerPlayer = document.getElementById('readerPlayer');
-  // var nameTxt = document.getElementById('songName');
-  // nameTxt.innerHTML = song['file_author'] + " - " + song['file_name'];
-  // musicPlayer.src = song['file_url'];
-  // var audioPlayer = document.getElementById('audio-player');
-  // audioPlayer.className = 'show';
-  // document.getElementById('Previous').onclick = function () {
-  //   mediaPlayerAppear(previous);
-  // };
-  // document.getElementById('Next').onclick = function () {
-  //   mediaPlayerAppear(next);
-  // };
+  var musicPlayer = document.getElementById('musicPlayer');
+  var readerPlayer = document.getElementById('readerPlayer');
+  var nameTxt = document.getElementById('songName');
+  nameTxt.innerHTML = author + " - " + name;
+  musicPlayer.src = url;
+  var audioPlayer = document.getElementById('audio-player');
+  audioPlayer.className = 'show';
+  document.getElementById('Previous').onclick = function () {
+    if (identifier != 0) {
+      identifier = identifier - 1;
+    }
+    mediaPlayerAppear(identifier);
+  };
+  document.getElementById('Next').onclick = function () {
+    if (document.getElementById(identifier+1) != undefined) {
+      identifier = identifier - 1;
+    }else {
+      identifier = 0;
+    }
+    mediaPlayerAppear(identifier);
+  };
 }
 
 function backgroundAppear(){
