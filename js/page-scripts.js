34de81d8f77b767588d8f1cfa0a959b64091291a
Pierre L'Hopital <lhopital.pierre@gmail.com>
2019-12-12 14:56:29 +0100

    prepare for applying next and previous song

--- js/page-scripts.js
+++ js/page-scripts.js
@@ -2,6 +2,8 @@ function mediaPlayerAppear(song, previous, next){
   var musicPlayer = document.getElementById('musicPlayer');
   var readerPlayer = document.getElementById('readerPlayer');
   var nameTxt = document.getElementById('songName');
+  console.log(song);
+  console.log(previous);
   nameTxt.innerHTML = song['file_author'] + " - " + song['file_name'];
   musicPlayer.src = song['file_url'];
   var audioPlayer = document.getElementById('audio-player');
