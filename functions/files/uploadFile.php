a08a983e1df242d8c6c30e45bbfb794d96862f02
Pierre L'Hopital <lhopital.pierre@gmail.com>
2019-12-12 18:45:57 +0100

    improve image upload

--- functions/files/uploadFile.php
+++ functions/files/uploadFile.php
@@ -1,12 +1,13 @@
 <?php
 
-  function uploadFile($file_path){
+  function uploadFile($file_path, $type){
     include_once($_SERVER['DOCUMENT_ROOT']."/functions/getCloudinary.php");
     $date = date_create();
     $name =  date_timestamp_get($date) . rand(1, 999999999);
     $result = \Cloudinary\Uploader::upload($file_path, array(
+              "folder" => $type."/",
               "public_id" => $name,
-              "resource_type" => "video"
+              "resource_type" => $type
     ));
     unlink($file_path);
     return $result['secure_url'];
@@ -15,29 +16,38 @@
   if (!empty($_FILES)) {
     $files = $_FILES["file"];
     $files = is_array($files) ? $files : array( $files );
-    $fileName = uploadFile($files["tmp_name"]);
+    $fileName = uploadFile($files["tmp_name"], 'video');
   }else {
     $fileName = "false";
   }
 
   if (isset($_FILES['picture']) && !empty($_FILES['picture'])) {
-    $file_name = $_FILES['picture']['name'];
-    $file_tmp = $_FILES['picture']['tmp_name'];
-    $type = pathinfo($file_tmp, PATHINFO_EXTENSION);
-    $data = file_get_contents( $file_tmp );
-    $base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
+    $img = $_FILES["picture"];
+    $img = is_array($img) ? $img : array( $img );
+
+    // include_once($_SERVER['DOCUMENT_ROOT']."/functions/compress.php");
+    // $img = compress($img, $destination_img, 90);
+
+    $imgName = uploadFile($img["tmp_name"], 'image');
+    // $file_name = $_FILES['picture']['name'];
+    // $file_tmp = $_FILES['picture']['tmp_name'];
+    // $type = pathinfo($file_tmp, PATHINFO_EXTENSION);
+    // $data = file_get_contents( $file_tmp );
+    // $base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
   }else {
-    $base64 = "";
+    $imgName = "";
   }
 
   if (!isset($_POST['album']) || $_POST['album'] == "") {
     $_POST['album'] = "indéfini";
   }
 
+  include_once($_SERVER['DOCUMENT_ROOT']."/functions/filter.php");
+  $filtered = map_entities($_POST);
 
   $db = connect();
   $selectSql = "INSERT INTO files (file_name, file_url, file_image, file_author, file_album) VALUES ($1, $2, $3, $4, $5)";
-  $result =  pg_query_params($db, $selectSql, array($_POST['name'], $fileName, $base64, $_POST['author'], $_POST['album']));
+  $result =  pg_query_params($db, $selectSql, array($filtered['name'], $fileName, $imgName, $filtered['author'], $filtered['album']));
 
 
 ?>
