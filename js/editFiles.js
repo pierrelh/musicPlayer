9f4722f180675496d9eb4a594228587ac2a8655d
Pierre L'Hopital <lhopital.pierre@gmail.com>
2019-12-13 14:34:09 +0100

    work on edit files

--- js/editFiles.js
+++ js/editFiles.js
@@ -1,10 +1,16 @@
 function editFiles(){
   var library = document.getElementById('Library').children;
-  for (var i = 0; i < library.length; i++) {
+  for (var i = 0; i < library.length; i++) (function(i) {
     var li = document.createElement('li');
     li.classList.add('edit');
+    li.id = 'edit' + i;
+
     var parent = document.getElementById('ul'+i);
     var child = document.getElementById(i);
     parent.insertBefore(li, child);
-  }
+
+    document.getElementById('edit'+i).onclick = function () {
+      alert(i);
+    };
+  })(i);
 }
