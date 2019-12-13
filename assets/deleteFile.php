6a5f7e728a2e806b544e0457e88cc608b129481c
Pierre L'Hopital <lhopital.pierre@gmail.com>
2019-12-13 15:23:39 +0100

    work on deleting files

---
+++ assets/deleteFile.php
@@ -0,0 +1,9 @@
+<link rel="stylesheet" href="<?php echo $link ?>/styles/deleteFileStyle.css">
+<section id="delete">
+  <h2>Voulez-vous vraiment supprimer le fichier ?</h2>
+  <ul>
+    <li><input type="button" name="" value="OUI"></li>
+    <li><input type="button" name="" value="NON"></li>
+  </ul>
+
+</section>
--- js/deleteFiles.js
+++ js/deleteFiles.js
@@ -1,3 +1,40 @@
 function deleteFiles(){
-  
+  var library = document.getElementById('Library').children;
+  for (var i = 0; i < library.length; i++) (function(i) {
+    var li = document.createElement('li');
+    li.classList.add('delete');
+    li.id = 'delete' + i;
+
+    var parent = document.getElementById('ul'+i);
+    var child = document.getElementById(i);
+    parent.insertBefore(li, child);
+
+    document.getElementById('delete'+i).onclick = function () {
+      showEditSection();
+    };
+  })(i);
+  var filter = document.getElementById('deleteFile');
+  filter.setAttribute('onclick', 'hideDelete()');
+}
+
+function hideDelete(){
+  var library = document.getElementById('Library').children;
+  for (var i = 0; i < library.length; i++){
+    var li = document.getElementById('delete'+i);
+    li.remove();
+  }
+  var filter = document.getElementById('deleteFile');
+  filter.setAttribute('onclick', 'deleteFiles()');
+}
+
+function showDeleteSection(){
+  backgroundAppear();
+  var del = document.getElementById('delete');
+  del.className = 'appear';
+}
+
+function hideDeleteSection(){
+  backgroundHide();
+  var del = document.getElementById('delete');
+  del.className = '';
 }
