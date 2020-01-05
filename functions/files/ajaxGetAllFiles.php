e95237cd4cb20511ccebfbbe022e353a06acfee1
Pierre L'Hopital <lhopital.pierre@gmail.com>
2020-01-05 15:52:03 +0100

    decode utf8

--- functions/files/ajaxGetAllFiles.php
+++ functions/files/ajaxGetAllFiles.php
@@ -1,5 +1,9 @@
 <?php
 
+  function ut8Decode($value){
+    return html_entity_decode($value);
+  }
+
   include_once($_SERVER['DOCUMENT_ROOT']."/functions/connexion.php");
   $db = connect();
   $row = $_POST['row'];
@@ -7,7 +11,8 @@
   $result = pg_query($db, "SELECT * FROM files ORDER BY ".$row." ".$type);
   if (!empty($result)) {
     $val = pg_fetch_all($result);
-    print json_encode($val);
+    $result = array_map('ut8Decode', $val);
+    print json_encode($result);
   }
 
 ?>
