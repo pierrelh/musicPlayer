4ef6c91a021a20c365738952c2efe0292c09cb35
Pierre L'Hopital <lhopital.pierre@gmail.com>
2020-01-25 16:16:10 +0100

    import playlist style

--- pages/index.php
+++ pages/index.php
@@ -9,6 +9,7 @@
     ?>
     <link rel="stylesheet" href="<?php echo $link ?>/styles/screen/videoStyle.css">
     <link rel="stylesheet" href="<?php echo $link ?>/styles/handheld/videoStyle.css">
+    <link rel="stylesheet" href="<?php echo $link ?>/styles/playlistStyle.css">
     <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js"></script>
     <script type="text/javascript" src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
     <script type="text/javascript" src="<?php echo $link ?>/js/getFiles.js"></script>
