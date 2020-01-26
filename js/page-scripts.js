5c95cf1652a005a6c66b734044511897b799bfcb
Pierre L'Hopital <lhopital.pierre@gmail.com>
2020-01-26 11:49:16 +0100

    work on playlist style

--- js/page-scripts.js
+++ js/page-scripts.js
@@ -7,6 +7,9 @@ function mediaPlayerAppear(identifier){
   var library = document.getElementById('Library');
   library.classList.add("library-reader-active");
 
+  var playlist = document.getElementById('divPlaylist');
+  playlist.classList.add('playlist-reader-showed');
+
   var musicPlayer = document.getElementById('musicPlayer');
   var readerPlayer = document.getElementById('readerPlayer');
   var nameTxt = document.getElementById('songName');
@@ -45,25 +48,37 @@ function backgroundHide(){
 
 function hideSidebar(){
   var arrow = document.getElementById('arrow');
+  arrow.className = 'arrow-active';
+  arrow.setAttribute('onclick', 'showSidebar()');
+
   var sidebar = document.getElementById('sidebar');
+  sidebar.className = 'sidebar-hide';
+
   var library = document.getElementById('Library');
+  library.classList.add('library-show');
+
+  var playlist = document.getElementById('divPlaylist');
+  playlist.classList.add('playlist-sidebar-hided');
+
   var player = document.getElementById('audio-player');
   player.classList.add("left");
-  arrow.className = 'arrow-active';
-  sidebar.className = 'sidebar-hide';
-  library.classList.add('library-show');
-  arrow.setAttribute('onclick', 'showSidebar()');
 }
 
 function showSidebar(){
   var arrow = document.getElementById('arrow');
-  var sidebar = document.getElementById('sidebar');
-  var library = document.getElementById('Library');
-  var player = document.getElementById('audio-player');
-  player.classList.remove("left");
   arrow.className = 'arrow';
+  arrow.setAttribute('onclick', 'hideSidebar()');
+
+  var sidebar = document.getElementById('sidebar');
   sidebar.className = 'sidebar';
+
+  var library = document.getElementById('Library');
   library.classList.add('library');
   library.classList.remove('library-show');
-  arrow.setAttribute('onclick', 'hideSidebar()');
+
+  var playlist = document.getElementById('divPlaylist');
+  playlist.classList.remove('playlist-sidebar-hided');
+
+  var player = document.getElementById('audio-player');
+  player.classList.remove("left");
 }
