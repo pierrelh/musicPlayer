34fb693108bb80e48ad3a30bb23792b55624ead2
Pierre L'Hopital <lhopital.pierre@gmail.com>
2020-02-02 15:43:39 +0100

    work on getting musics from playlists

--- functions/files/getFilesById.php
+++ functions/files/getFilesById.php
@@ -3,6 +3,8 @@
   include_once($_SERVER['DOCUMENT_ROOT']."/functions/connexion.php");
   $db = connect();
   $ids = json_decode('[' . $_POST['ids'] . ']', true);
+  array_pop($ids);
+  error_log("ids: ". $ids);
   $result = pg_query($db, "SELECT * FROM files WHERE file_id IN ($ids)");
   if (!empty($result)) {
     $val = pg_fetch_all($result);
