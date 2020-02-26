fc463f01ecfc404a7faf5e24b38d748437b60ba3
Pierre L'Hopital <lhopital.pierre@gmail.com>
2020-02-26 11:56:27 +0100

    fix volume slider refresh style

--- js/musicPlayer.js
+++ js/musicPlayer.js
@@ -67,7 +67,7 @@ $(document).ready(function () {
     $(mute_button).toggleClass("fa-volume-off", player.volume == 0);
   }, false);
 
-  $("#Volume").onchange(function (e) {
+  $("#Volume").change(function (e) {
     var val = ($(this).val() - $(this).attr('min')) / ($(this).attr('max') - $(this).attr('min'));
     var percent = val * 100;
 
