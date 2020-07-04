b9eb36cb9db381bfef6f64c72678ce43807b8d07
Pierre L'Hopital <lhopital.pierre@gmail.com>
2020-07-04 18:24:30 +0200

    rework upload files

--- functions/files/uploadFile.php
+++ functions/files/uploadFile.php
@@ -14,7 +14,7 @@
   include_once($_SERVER['DOCUMENT_ROOT']."/functions/connexion.php");
   $db = connect();
   $selectSql = "INSERT INTO files (file_name, file_url, file_image, file_author, file_album) VALUES ($1, $2, $3, $4, $5)";
-  $result =  pg_query_params($db, $selectSql, array($filtered['file_name'], $_POST['file'], $_POST['picture'], $filtered['file_author'], $filtered['file_album']));
+  $result =  pg_query_params($db, $selectSql, array($filtered['file_name'], $_POST['file_url'], $_POST['file_image'], $filtered['file_author'], $filtered['file_album']));
 
 
 ?>
--- js/uploadFile.js
+++ js/uploadFile.js
@@ -74,7 +74,6 @@ document.getElementById("barSpan2").addEventListener("click", function(){
         var response = JSON.parse(audioFileXhr.responseText);
         formData.set('file_url', response.secure_url)
         document.getElementById('textProgressBarVideo').innerHTML = "Envoyé";
-
         pictureFile.append('upload_preset', 'unsigned_image');
         pictureFile.append('tags', 'browser_upload'); // Optional - add tag for image admin in Cloudinary
         pictureFile.append('file', picture);
@@ -85,15 +84,8 @@ document.getElementById("barSpan2").addEventListener("click", function(){
             formData.set('file_image', response.public_id)
             document.getElementById('textProgessBarPicture').innerHTML = "Envoyé";
             uploadFile();
-          }else {
-            console.log("Failed to upload picture");
-            return;
           }
         };
-
-      }else {
-        console.log("Failed to upload video.")
-        return;
       }
     };
   }else {
@@ -102,72 +94,3 @@ document.getElementById("barSpan2").addEventListener("click", function(){
     document.getElementById("error-msg").appendChild(errormsg);
   }
 });
-//
-//
-// function uploadFile(){
-//   var file = document.getElementById("file").dataset.name;
-//   var picture = document.getElementById("picture").dataset.name;
-//   var name = document.getElementById('fileName').value;
-//   var author = document.getElementById('fileAuthor').value;
-//   var album = document.getElementById('fileAlbum').value;
-//   if (file == "" || file == undefined) {
-//     if (document.getElementById('error-msg').innerHTML == "") {
-//       var errormsg = document.createTextNode("Aucun fichier n'a été choisi.");
-//       document.getElementById("error-msg").appendChild(errormsg);
-//     }
-//   }else if (file == "uploading" || picture == "uploading") {
-//     document.getElementById('error-msg').innerHTML = ""
-//     var errormsg = document.createTextNode("Veuillez attendre que les fichiers aient fini d'être uploadé.");
-//     document.getElementById("error-msg").appendChild(errormsg);
-//   }else if (name == '' || author == '') {
-//     document.getElementById('error-msg').innerHTML = ""
-//     var errormsg = document.createTextNode("Merci de remplir tous les champs.");
-//     document.getElementById("error-msg").appendChild(errormsg);
-//   }else{
-//     var form_data = new FormData($('formUpload')[0]);
-//     form_data.append('file', file);
-//     form_data.append('picture', picture);
-//     form_data.append('name', name);
-//     form_data.append('author', author);
-//     form_data.append('album', album);
-//     $.ajax({
-//       url: "../functions/files/uploadFile.php",
-//       type: "POST",
-//       dataType: 'script',
-//       cache: false,
-//       contentType: false,
-//       processData: false,
-//       data: form_data,
-//       xhr: function () {
-//         var xhr = $.ajaxSettings.xhr();
-//         xhr.upload.onprogress = function (e) {
-//           if (e.lengthComputable) {
-//             var elem = document.getElementById("myBarPlus");
-//             var elem1 = document.getElementById("myBarMoins");
-//             var span = document.getElementById("sendButton");
-//             var span1 = document.getElementById("barSpan2");
-//             elem.style.width = Math.round((e.loaded / e.total)*100) + "%";
-//             span.innerHTML = Math.round((e.loaded / e.total)*100) + " %";
-//             elem1.style.width = Math.round(100-(e.loaded / e.total)*100) + "%";
-//             span1.innerHTML = Math.round((e.loaded / e.total)*100) + " %";
-//           }
-//         };
-//         return xhr;
-//       }
-//     }).done(function (e) {
-//         alert("upload succeed")
-//         getFiles('file_id', 'DESC');
-//         var library = document.getElementById('LibraryObjects');
-//         var elem = document.getElementById("myBarPlus");
-//         var elem1 = document.getElementById("myBarMoins");
-//         var span = document.getElementById("sendButton");
-//         var span1 = document.getElementById("barSpan2");
-//         elem.style.width = "0%";
-//         span.innerHTML = "0 %";
-//         elem1.style.width = "100%";
-//         span1.innerHTML = "ENVOYER";
-//     }).fail(function (e) {
-//         alert("upload failed");
-//     });
-//   }
-// }
