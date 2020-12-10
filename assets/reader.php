e651797179833260179a028dbd5153c2c87f6bf8
Pierre L'Hopital <lhopital.pierre@gmail.com>
2020-12-10 18:38:06 +0100

    work on reader style

--- assets/reader.php
+++ assets/reader.php
@@ -6,13 +6,13 @@
   <section id="controls">
     <img id="Loop" data-loop="all" class="reader-buttons loop" src="../../img/loop.png" alt="loop"><!-- @whitespace
     --><img id="Previous" class='reader-buttons' src="../img/back.png" alt=""><!-- @whitespace
-    --><img id="PlayPause" data-is-playing="true" class="reader-buttons" src="../../img/pause.png" alt="play"><!-- @whitespace
+    --><img id="PlayPause" data-is-playing="true" class="reader-buttons play" src="../../img/pause.png" alt="play"><!-- @whitespace
     --><img id="Next" class='reader-buttons' src="../img/skip.png" alt=""><!-- @whitespace
-    --><span id="start" class="time">00:00</span><!-- @whitespace
+    --><span id="start" class="time current-time">00:00</span><!-- @whitespace
     --><input type="range" min="0" max="100" value="0" id="progress-bar" name="" value=""><!-- @whitespace
-    --><span id="time" class="time">00:00</span><!-- @whitespace
-    --><img id="Random" data-random="false" class="reader-buttons random" src="../../img/no-random.png" alt="random"><!-- @whitespace
-    --><img id="Mute" data-mute="false" data-volume="1" class="reader-buttons" src="../../img/audio-on.png" alt=""><!-- @whitespace
+    --><span id="time" class="time total-time">00:00</span><!-- @whitespace
+    --><img id="Random" data-random="false" class="reader-buttons" src="../../img/no-random.png" alt="random"><!-- @whitespace
+    --><img id="Mute" data-mute="false" data-volume="1" class="reader-buttons mute" src="../../img/audio-on.png" alt=""><!-- @whitespace
     --><input type="range" min="0" max="100" value="100" id="Volume" name="" value="">
   </section>
 </section>
