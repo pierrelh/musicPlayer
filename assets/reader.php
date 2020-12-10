1d842e2538ab8f919177a858f4d689aea238a18a
Pierre L'Hopital <lhopital.pierre@gmail.com>
2020-12-10 17:18:07 +0100

    work on reader style

--- assets/reader.php
+++ assets/reader.php
@@ -2,24 +2,18 @@
 <link rel="stylesheet" href="<?php echo $link ?>/styles/screen/readerStyle.css">
 <link rel="stylesheet" href="<?php echo $link ?>/styles/handheld/readerStyle.css">
 <section id="audio-player" class="left">
-  <ul>
-    <li>
-      <p id="songName"></p>
-    </li>
-    <li>
-      <div id="controls">
-        <img id="Loop" data-loop="all" class="reader-buttons" src="../../img/loop.png" alt="loop">
-        <img id="Previous" class='reader-buttons' src="../img/back.png" alt="">
-        <img id="PlayPause" data-is-playing="true" class="reader-buttons" src="../../img/pause.png" alt="play">
-        <img id="Next" class='reader-buttons' src="../img/skip.png" alt="">
-        <span id="start" class="time">00:00</span>
-        <input type="range" min="0" max="100" value="0" id="progress-bar" name="" value="">
-        <span id="time" class="time">00:00</span>
-        <img id="Random" data-random="false" class="reader-buttons" src="../../img/no-random.png" alt="random">
-        <img id="Mute" data-mute="false" data-volume="1" class="reader-buttons" src="../../img/audio-on.png" alt="">
-        <input type="range" min="0" max="100" value="100" id="Volume" name="" value="">
-      </div>
-    </li>
-  </ul>
+  <p id="songName"></p>
+  <div id="controls">
+    <img id="Loop" data-loop="all" class="reader-buttons" src="../../img/loop.png" alt="loop">
+    <img id="Previous" class='reader-buttons' src="../img/back.png" alt="">
+    <img id="PlayPause" data-is-playing="true" class="reader-buttons" src="../../img/pause.png" alt="play">
+    <img id="Next" class='reader-buttons' src="../img/skip.png" alt="">
+    <span id="start" class="time">00:00</span>
+    <input type="range" min="0" max="100" value="0" id="progress-bar" name="" value="">
+    <span id="time" class="time">00:00</span>
+    <img id="Random" data-random="false" class="reader-buttons" src="../../img/no-random.png" alt="random">
+    <img id="Mute" data-mute="false" data-volume="1" class="reader-buttons" src="../../img/audio-on.png" alt="">
+    <input type="range" min="0" max="100" value="100" id="Volume" name="" value="">
+  </div>
 </section>
 <script type="text/javascript" src="<?php echo $link ?>/js/reader.js"></script>
