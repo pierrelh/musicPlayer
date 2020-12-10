b7566c643a68c9f11421e18dd5d1ca3f78de4d26
Pierre L'Hopital <lhopital.pierre@gmail.com>
2020-12-10 16:01:32 +0100

    rework reader functions

--- assets/reader.php
+++ assets/reader.php
@@ -16,7 +16,7 @@
         <input type="range" min="0" max="100" value="0" id="progress-bar" name="" value="">
         <span id="time" class="time">00:00</span>
         <img id="Random" data-random="false" class="fa-no-random" src="../../img/no-random.png" alt="random">
-        <img id="Mute" data-mute="false" data-volume="100" class="fa-volume-up" src="../../img/audio-on.png" alt="">
+        <img id="Mute" data-mute="false" data-volume="1" class="fa-volume-up" src="../../img/audio-on.png" alt="">
         <input type="range" min="0" max="100" value="100" id="Volume" name="" value="">
       </div>
     </li>
