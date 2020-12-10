4dc63111aeac827c7f4323859e637737083c3b68
Pierre L'Hopital <lhopital.pierre@gmail.com>
2020-12-10 18:07:52 +0100

    work on reader style

--- assets/reader.php
+++ assets/reader.php
@@ -4,14 +4,14 @@
 <section id="audio-player" class="left">
   <p id="songName"></p>
   <div id="controls">
-    <img id="Loop" data-loop="all" class="reader-buttons" src="../../img/loop.png" alt="loop">
+    <img id="Loop" data-loop="all" class="reader-buttons loop" src="../../img/loop.png" alt="loop">
     <img id="Previous" class='reader-buttons' src="../img/back.png" alt="">
     <img id="PlayPause" data-is-playing="true" class="reader-buttons" src="../../img/pause.png" alt="play">
     <img id="Next" class='reader-buttons' src="../img/skip.png" alt="">
     <span id="start" class="time">00:00</span>
     <input type="range" min="0" max="100" value="0" id="progress-bar" name="" value="">
     <span id="time" class="time">00:00</span>
-    <img id="Random" data-random="false" class="reader-buttons" src="../../img/no-random.png" alt="random">
+    <img id="Random" data-random="false" class="reader-buttons random" src="../../img/no-random.png" alt="random">
     <img id="Mute" data-mute="false" data-volume="1" class="reader-buttons" src="../../img/audio-on.png" alt="">
     <input type="range" min="0" max="100" value="100" id="Volume" name="" value="">
   </div>
