640cb1ef6203b817c6c763580ef34f571e77c150
Pierre L'Hopital <lhopital.pierre@gmail.com>
2020-12-16 22:46:46 +0100

    work on account asset

--- assets/account.php
+++ assets/account.php
@@ -2,36 +2,37 @@
 <link rel="stylesheet" href="<?php echo $link ?>/styles/screen/accountStyle.css">
 <link rel="stylesheet" href="<?php echo $link ?>/styles/handheld/accountStyle.css">
 <section id="Account">
-<?php
-    include_once($_SERVER['DOCUMENT_ROOT']."/functions/getCloudinary.php");
+    <img id="CrossAccount" class="cross" src="../img/cross.png" alt="">
+    <?php
+        include_once($_SERVER['DOCUMENT_ROOT']."/functions/getCloudinary.php");
 
-    function get_web_page($url) {
-        $options = array(
-            CURLOPT_RETURNTRANSFER => true,   // return web page
-            CURLOPT_HEADER         => false,  // don't return headers
-            CURLOPT_FOLLOWLOCATION => true,   // follow redirects
-            CURLOPT_MAXREDIRS      => 10,     // stop after 10 redirects
-            CURLOPT_ENCODING       => "",     // handle compressed
-            CURLOPT_USERAGENT      => "test", // name of client
-            CURLOPT_AUTOREFERER    => true,   // set referrer on redirect
-            CURLOPT_CONNECTTIMEOUT => 120,    // time-out on connect
-            CURLOPT_TIMEOUT        => 120,    // time-out on response
-        ); 
+        function get_web_page($url) {
+            $options = array(
+                CURLOPT_RETURNTRANSFER => true,   // return web page
+                CURLOPT_HEADER         => false,  // don't return headers
+                CURLOPT_FOLLOWLOCATION => true,   // follow redirects
+                CURLOPT_MAXREDIRS      => 10,     // stop after 10 redirects
+                CURLOPT_ENCODING       => "",     // handle compressed
+                CURLOPT_USERAGENT      => "test", // name of client
+                CURLOPT_AUTOREFERER    => true,   // set referrer on redirect
+                CURLOPT_CONNECTTIMEOUT => 120,    // time-out on connect
+                CURLOPT_TIMEOUT        => 120,    // time-out on response
+            ); 
 
-        $ch = curl_init($url);
-        curl_setopt_array($ch, $options);
+            $ch = curl_init($url);
+            curl_setopt_array($ch, $options);
 
-        $content  = curl_exec($ch);
+            $content  = curl_exec($ch);
 
-        curl_close($ch);
+            curl_close($ch);
 
-        return $content;
-    }
-    
-    $response = get_web_page("https://$key:$secret@api.cloudinary.com/v1_1/$name/usage");
-    $resArr = array();
-    $resArr = json_decode($response);
-    echo "<pre>"; print_r($resArr); echo "</pre>";
-?>
+            return $content;
+        }
 
+        $response = get_web_page("https://$key:$secret@api.cloudinary.com/v1_1/$name/usage");
+        $resArr = array();
+        $resArr = json_decode($response);
+        echo "<p>" . $resArr["plan"] . "</p>";
+    ?>
+	<script type="text/javascript" src="<?php echo $link ?>/js/account.js"></script>
 </section>
---
+++ js/account.js
@@ -0,0 +1,6 @@
+// Handle click on CrossAccount button of Account section
+document.getElementById("CrossAccount").addEventListener("click", function() {
+	backgroundHide();
+	var Account = document.getElementById("Account");
+	Account.className = "";
+});
