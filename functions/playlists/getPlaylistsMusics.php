ed9c5d05df7a2658cd6cbb33b2b42245ef74eecd
Pierre L'Hopital <lhopital.pierre@gmail.com>
2020-12-16 21:33:18 +0100

    rework playlists functions

--- functions/playlists/getPlaylistsMusics.php
+++ functions/playlists/getPlaylistsMusics.php
@@ -2,17 +2,21 @@
 
 	include_once($_SERVER['DOCUMENT_ROOT']."/functions/connexion.php");
 	$db = connect();
-	// $request = "";
-    // $result = pg_query_params(
-    //     $db,
-    //     $request,
-    //     array(
-
-    //     )
-    // );
-	// if (!empty($result)) {
-	// 	$val = pg_fetch_all($result);
-	// 	print json_encode($val);
-	// }
+	$request = "SELECT *
+                FROM playlists_musics
+                INNER JOIN files
+                ON playlists_musics.playlist_music_id = files.file_id
+                WHERE playlist_id = $1;";
+    $result = pg_query_params(
+        $db,
+        $request,
+        array(
+            $_POST["playlist_id"]
+        )
+    );
+	if (!empty($result)) {
+		$val = pg_fetch_all($result);
+		print json_encode($val);
+	}
 
 ?>
