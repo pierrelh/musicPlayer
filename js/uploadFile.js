5672c25fef7eb3f9592137d6b2d6bddabfc0b545
Pierre L'Hopital <lhopital.pierre@gmail.com>
2020-12-16 13:13:55 +0100

    rework upload functions

--- js/uploadFile.js
+++ js/uploadFile.js
@@ -1,16 +1,16 @@
-document.getElementById("CrossUpload").addEventListener("click", function () {
-	backgroundHide();
-	var Upload = document.getElementById("Upload");
-	Upload.className = "";
-});
-
+// Function to upload a music
 async function uploadMusic() {
 	var file = document.getElementById("File").files[0];
 	var picture = document.getElementById("Picture").files[0];
+
+	// Check if file & picture exist
 	if (file != undefined && picture != undefined) {
 		var formData = new FormData(document.getElementById("FormUpload"));
+		// Delete default from data values for file & picture
 		formData.delete("file");
 		formData.delete("picture");
+
+		// Check if the music has a name & an author
 		if (document.getElementById("FileName").value == "" || document.getElementById("FileAuthor").value == "") {
 			document.getElementById("ErrorMsgUpload").innerHTML = ""
 			var errormsg = document.createTextNode("Merci de remplir tous les champs.");
@@ -18,16 +18,19 @@ async function uploadMusic() {
 			return;
 		}
 
+		// Uploadind the music to cloudinary
 		await uploadFileCloudinary(file, "unsigned_video", "ProgressBarVideo", "TextProgressBarVideo").then( async (audioFileXhr) => {
 			var response = JSON.parse(audioFileXhr.responseText);
 			formData.set("file_url", response.secure_url)
 			document.getElementById("TextProgressBarVideo").innerHTML = "Envoyé";
 
+			// Uploading the cover to cloudinary
 			await uploadFileCloudinary(picture, "unsigned_image", "ProgressBarPicture", "TextProgessBarPicture").then((pictureFileXhr) => {
 				var response = JSON.parse(pictureFileXhr.responseText);
 				formData.set("file_image", response.secure_url)
 				document.getElementById("TextProgessBarPicture").innerHTML = "Envoyé";
 				
+				// Sending the datas to the db
 				$.ajax({
 					url: server + "/functions/files/uploadFile.php",
 					type: "POST",
@@ -37,7 +40,6 @@ async function uploadMusic() {
 					processData: false,
 					data: formData,
 					success: function () {
-						alert("upload succeed")
 						getFiles("file_id", "DESC");
 					}
 				});
@@ -51,4 +53,12 @@ async function uploadMusic() {
 	}
 }
 
+// Handle click event on CrossUpload
+document.getElementById("CrossUpload").addEventListener("click", function () {
+	backgroundHide();
+	var Upload = document.getElementById("Upload");
+	Upload.className = "";
+});
+
+// Handle click on BarSpan2
 document.getElementById("BarSpan2").addEventListener("click", uploadMusic, false);
