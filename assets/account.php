996fd44ac1491c7c0d8dcf0b9c3414f23599b591
Pierre L'Hopital <lhopital.pierre@gmail.com>
2020-12-16 22:39:51 +0100

    work on account asset

--- assets/account.php
+++ assets/account.php
@@ -4,11 +4,34 @@
 <section id="Account">
 <?php
     include_once($_SERVER['DOCUMENT_ROOT']."/functions/getCloudinary.php");
-    $result = $api->resources();
-    var_dump($result->rate_limit_allowed);
-    var_dump($result->rate_limit_remaining);
-    var_dump($result->rate_limit_reset_at);
-    var_dump($result);
+
+    function get_web_page($url) {
+        $options = array(
+            CURLOPT_RETURNTRANSFER => true,   // return web page
+            CURLOPT_HEADER         => false,  // don't return headers
+            CURLOPT_FOLLOWLOCATION => true,   // follow redirects
+            CURLOPT_MAXREDIRS      => 10,     // stop after 10 redirects
+            CURLOPT_ENCODING       => "",     // handle compressed
+            CURLOPT_USERAGENT      => "test", // name of client
+            CURLOPT_AUTOREFERER    => true,   // set referrer on redirect
+            CURLOPT_CONNECTTIMEOUT => 120,    // time-out on connect
+            CURLOPT_TIMEOUT        => 120,    // time-out on response
+        ); 
+
+        $ch = curl_init($url);
+        curl_setopt_array($ch, $options);
+
+        $content  = curl_exec($ch);
+
+        curl_close($ch);
+
+        return $content;
+    }
+    
+    $response = get_web_page("https://$key:$secret@api.cloudinary.com/v1_1/$name/usage");
+    $resArr = array();
+    $resArr = json_decode($response);
+    echo "<pre>"; print_r($resArr); echo "</pre>";
 ?>
-<script type="text/javascript" src="<?php echo $link ?>/js/uploadFile.js"></script>
+
 </section>
