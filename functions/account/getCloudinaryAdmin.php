9323251265e8935af1643e34bb88321636974eab
Pierre L'Hopital <lhopital.pierre@gmail.com>
2020-12-17 16:56:29 +0100

    working on improving cUrl request

--- functions/account/getCloudinaryAdmin.php
+++ functions/account/getCloudinaryAdmin.php
@@ -6,8 +6,5 @@
     $response = getWebPage("https://$key:$secret@api.cloudinary.com/v1_1/$name/usage");
     
     print $response;
-    // $result = json_decode($response);
-    // $result = get_object_vars($result);
-    
 
 ?>
--- functions/getWebPage.php
+++ functions/getWebPage.php
@@ -1,16 +1,18 @@
 <?php
 
+    // Get the content of a web page with cUrl
     function getWebPage($url) {
         $options = array(
             CURLOPT_RETURNTRANSFER => true,   // return web page
             CURLOPT_HEADER         => false,  // don't return headers
-            CURLOPT_FOLLOWLOCATION => true,   // follow redirects
+            CURLOPT_NOBODY         => true,   // don't return body
+            CURLOPT_FOLLOWLOCATION => false,  // don't follow redirects
             CURLOPT_MAXREDIRS      => 10,     // stop after 10 redirects
             CURLOPT_ENCODING       => "",     // handle compressed
             CURLOPT_USERAGENT      => "test", // name of client
             CURLOPT_AUTOREFERER    => true,   // set referrer on redirect
-            CURLOPT_CONNECTTIMEOUT => 120,    // time-out on connect
-            CURLOPT_TIMEOUT        => 120,    // time-out on response
+            CURLOPT_CONNECTTIMEOUT => 10,    // time-out on connect
+            CURLOPT_TIMEOUT        => 10,    // time-out on response
         ); 
 
         $ch = curl_init($url);
