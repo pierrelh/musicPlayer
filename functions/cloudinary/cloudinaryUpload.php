013f6825076d55c6ee7ad48dafba531cd0271a15
Pierre L'Hopital <lhopital.pierre@gmail.com>
2020-12-18 15:20:14 +0100

    rework upload functions

--- functions/cloudinary/cloudinaryUpload.php
+++ functions/cloudinary/cloudinaryUpload.php
@@ -1,36 +1,36 @@
 <?php
 
-function uploadCover($file_path, $overwrite){
-    include_once($_SERVER['DOCUMENT_ROOT']."/functions/cloudinary/setCloudinary.php");
-    $result = \Cloudinary\Uploader::upload(
-        $file_path,
-        array(
-            "resource_type" => "auto",
-            "folder" => "image",
-            "overwrite" => $overwrite,
-            "eager" => array(
-                "width" => 150,
-                "height"=> 150,
-                "crop" => "scale" 
-            ),
-            "quality" => "auto"
-        )
-    );
-    return $result['secure_url'];
-}
+    function uploadCover($file_path, $overwrite){
+        include_once($_SERVER['DOCUMENT_ROOT']."/functions/cloudinary/setCloudinary.php");
+        $result = \Cloudinary\Uploader::upload(
+            $file_path,
+            array(
+                "resource_type" => "auto",
+                "folder" => "image",
+                "overwrite" => $overwrite,
+                "eager" => array(
+                    "width" => 150,
+                    "height"=> 150,
+                    "crop" => "scale" 
+                ),
+                "quality" => "auto"
+            )
+        );
+        return $result['secure_url'];
+    }
 
-function uploadMusic($fileName){
-    include_once($_SERVER['DOCUMENT_ROOT']."/functions/cloudinary/setCloudinary.php");
-    $result = \Cloudinary\Uploader::upload(
-        $fileName,
-        array(
-            "public_id" => $fileName,
-            "resource_type" => "auto",
-            "folder" => "video",
-            "overwrite" => false
-        )
-    );
-    return $result['secure_url'];
-}
+    function uploadMusic($filePath, $fileName){
+        include_once($_SERVER['DOCUMENT_ROOT']."/functions/cloudinary/setCloudinary.php");
+        $result = \Cloudinary\Uploader::upload(
+            $filePath,
+            array(
+                "public_id" => $fileName,
+                "resource_type" => "auto",
+                "folder" => "video",
+                "overwrite" => false
+            )
+        );
+        return $result['secure_url'];
+    }
 
 ?>
--- functions/files/uploadMusic.php
+++ functions/files/uploadMusic.php
@@ -1,7 +1,8 @@
 <?php
 
     include_once($_SERVER['DOCUMENT_ROOT']."/functions/cloudinary/cloudinaryUpload.php");
-    error_log( print_r($_FILES["music"], TRUE) );
-    // uploadMusic($_FILES["music"]["tmp_name"]);
+    
+    $music = $_FILES["music"]; 
+    uploadMusic($music["tmp_name"], $music["name"]);
 
 ?>
