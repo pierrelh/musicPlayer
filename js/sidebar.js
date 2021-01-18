f6988aadb5d47a5ff6a9f45761209c8b0a478afd
Pierre L'Hopital <lhopital.pierre@gmail.com>
2021-01-18 17:43:57 +0100

    rework edit button action

--- js/sidebar.js
+++ js/sidebar.js
@@ -69,33 +69,74 @@ UploadFileSidebar.addEventListener("click", function() {
 
 // Handle the edit file button click
 EditFileSidebar.addEventListener("click", function() {
-	var library = document.getElementById("LibraryObjects").children;
-	for (var i = 0; i < library.length; i++) (function(i) {
-		// Check if library is already in editing mode
-		if (document.getElementById("Edit" + i) != undefined) {
-			document.getElementById("Edit" + i).remove();
-		}else {
-			// Check if library is already on delete or add to playlist mode & remove it if needed
-			if (document.getElementById("Delete" + i) != undefined) {
-				document.getElementById("Delete" + i).remove();
-			}else if (document.getElementById("Add" + i) != undefined) {
-				document.getElementById("Add" + i).remove();
+	// If elements with edit's class exists then delete them & quit
+	var editElements = document.getElementsByClassName("edit");
+	if (editElements.length != 0) {
+		for (var index = 0; index < editElements.length; index++) {
+			editElements[index].remove;			
+		}
+		return;
+	}else {
+		// If elements with delete's class exists then delete them
+		var deleteElements = document.getElementsByClassName("delete");
+		if (deleteElements.length != 0) {
+			for (var index = 0; index < deleteElements.length; index++) {
+				deleteElements[index].remove;			
 			}
+		}else {
+			// If elements with add's class exists then delete them
+			var addElements = document.getElementsByClassName("add");
+			if (addElements.length != 0) {
+				for (var index = 0; index < addElements.length; index++) {
+					addElements[index].remove;			
+				}
+			}		
+		}
 
-			// Creating the Delete element & add it to the music's parent
+		// Create the edit elements
+		var libraryChildren = document.getElementById("LibraryObjects").children;
+		for (var i = 0; i < libraryChildren.length; i++) (function(i){
 			var li = document.createElement("li");
 			li.classList.add("edit");
 			li.id = "Edit" + i;
-			var parent = document.getElementById("MusicList" + i);
-			var child = document.getElementById("Music" + i);
+			var parent = libraryChildren[i];
+			var child = parent.children[0];
 			parent.insertBefore(li, child);
 
 			// Add the event on this delete's click
 			document.getElementById("Edit" + i).onclick = function () {
 				showEditSection(i);
 			};
-		}        
-	})(i);
+			
+		})(i)
+	}
+	// var library = document.getElementById("LibraryObjects").children;
+	// for (var i = 0; i < library.length; i++) (function(i) {
+	// 	// Check if library is already in editing mode
+	// 	if (document.getElementById("Edit" + i) != undefined) {
+	// 		document.getElementById("Edit" + i).remove();
+	// 	}else {
+	// 		// Check if library is already on delete or add to playlist mode & remove it if needed
+	// 		if (document.getElementById("Delete" + i) != undefined) {
+	// 			document.getElementById("Delete" + i).remove();
+	// 		}else if (document.getElementById("Add" + i) != undefined) {
+	// 			document.getElementById("Add" + i).remove();
+	// 		}
+
+	// 		// Creating the Delete element & add it to the music's parent
+	// 		var li = document.createElement("li");
+	// 		li.classList.add("edit");
+	// 		li.id = "Edit" + i;
+	// 		var parent = document.getElementById("MusicList" + i);
+	// 		var child = document.getElementById("Music" + i);
+	// 		parent.insertBefore(li, child);
+
+	// 		// Add the event on this delete's click
+	// 		document.getElementById("Edit" + i).onclick = function () {
+	// 			showEditSection(i);
+	// 		};
+	// 	}        
+	// })(i);
 });
 
 // Handle the delete file button click
