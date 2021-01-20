6ba2b625199d48e516627363dc07f3f739fe13d8
Pierre L'Hopital <lhopital.pierre@gmail.com>
2021-01-20 17:43:58 +0100

    rework master.css & readerStyle.css

--- assets/headers/header.php
+++ assets/headers/header.php
@@ -1,15 +1,12 @@
-<!-- Import Common Styles -->
 <link rel="stylesheet" href="<?php echo $link ?>/styles/common/master.css">
+<link rel="stylesheet" href="<?php echo $link ?>/styles/screen/master.css">
+<link rel="stylesheet" href="<?php echo $link ?>/styles/handheld/master.css">
 <link rel="stylesheet" href="<?php echo $link ?>/styles/common/headerStyle.css">
+<link rel="stylesheet" href="<?php echo $link ?>/styles/screen/headerStyle.css">
+<link rel="stylesheet" href="<?php echo $link ?>/styles/handheld/headerStyle.css">
 <link rel="stylesheet" href="<?php echo $link ?>/styles/common/videoStyle.css">
-<!-- Import Standart Screen Styles -->
-<link rel="stylesheet" media="screen and (min-device-width: 641px)" href="<?php echo $link ?>/styles/screen/master.css" />
-<link rel="stylesheet" media="screen and (min-device-width: 641px)" href="<?php echo $link ?>/styles/screen/headerStyle.css" />
-<link rel="stylesheet" media="screen and (min-device-width: 641px)" href="<?php echo $link ?>/styles/screen/videoStyle.css" />
-<!-- Import Small Screen Styles -->
-<link rel="stylesheet" media="screen and (max-device-width: 640px)" href="<?php echo $link ?>/styles/handheld/master.css" />
-<link rel="stylesheet" media="screen and (max-device-width: 640px)" href="<?php echo $link ?>/styles/handheld/headerStyle.css" />
-<link rel="stylesheet" media="screen and (max-device-width: 640px)" href="<?php echo $link ?>/styles/handheld/videoStyle.css" />
+<link rel="stylesheet" href="<?php echo $link ?>/styles/screen/videoStyle.css">
+<link rel="stylesheet" href="<?php echo $link ?>/styles/handheld/videoStyle.css">
 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
