10862745edeb0024994d32231f16c1367950fae4
Pierre L'Hopital <lhopital.pierre@gmail.com>
2021-01-22 18:11:25 +0100

    rework playlists style for screens

--- assets/playlists.php
+++ assets/playlists.php
@@ -1,3 +1,5 @@
 <script type="text/javascript" src="<?php echo $link ?>/js/playlist.js"></script>
 <link rel="stylesheet" href="<?php echo $link ?>/styles/common/playlistStyle.css">
+<link rel="stylesheet" href="<?php echo $link ?>/styles/screen/playlistStyle.css">
+<link rel="stylesheet" href="<?php echo $link ?>/styles/handheld/playlistStyle.css">
 <section id="DivPlaylist" class="playlist-div-hide playlist-sidebar-hided"></section>
