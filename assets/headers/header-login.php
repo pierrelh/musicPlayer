fee7431aa0c67fecb9b5b053a095ee0ecf61ea63
Pierre L'Hopital <lhopital.pierre@gmail.com>
2021-01-22 18:37:53 +0100

    rework login style for screens

--- assets/headers/header-login.php
+++ assets/headers/header-login.php
@@ -6,6 +6,8 @@
 <link rel="stylesheet" href="<?php echo $link ?>/styles/screen/loginStyle.css">
 <link rel="stylesheet" href="<?php echo $link ?>/styles/handheld/loginStyle.css">
 <link rel="stylesheet" href="<?php echo $link ?>/styles/common/forms.css">
+<link rel="stylesheet" href="<?php echo $link ?>/styles/screen/forms.css">
+<link rel="stylesheet" href="<?php echo $link ?>/styles/handheld/forms.css">
 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
