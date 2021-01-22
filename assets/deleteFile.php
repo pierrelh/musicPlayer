ea181c7b8bcd1c3a0e223e82fd746e83ea8e91b3
Pierre L'Hopital <lhopital.pierre@gmail.com>
2021-01-22 17:29:04 +0100

    rework delete file for screens

--- assets/deleteFile.php
+++ assets/deleteFile.php
@@ -2,7 +2,7 @@
 <link rel="stylesheet" href="<?php echo $link ?>/styles/handheld/deleteFileStyle.css">
 <link rel="stylesheet" href="<?php echo $link ?>/styles/screen/deleteFileStyle.css">
 <section id="Delete">
-	<h2 id="DeleteTitle"></h2>
+	<h2 class="form-title" id="DeleteTitle"></h2>
 	<ul>
 		<li><input id="DeleteYes" type="button" name="" value="OUI"></li>
 		<li><input id="DeleteNo" type="button" name="" value="NON"></li>
