a4c92046a98677d2234237a035482c6c3a22916c
Pierre L'Hopital <lhopital.pierre@gmail.com>
2021-01-26 15:24:38 +0100

    fix media session handler

--- js/media-session.js
+++ js/media-session.js
@@ -1,26 +1,42 @@
 if ('mediaSession' in navigator) {
     
     // Handle the play action on mediaSession
-    navigator.mediaSession.setActionHandler("play", togglePlayPause);
+    navigator.mediaSession.setActionHandler('play', function() {
+        togglePlayPause();
+    });
 
     // Handle the pause action on mediaSession
-    navigator.mediaSession.setActionHandler("pause", togglePlayPause);
+    navigator.mediaSession.setActionHandler('pause', function() {
+        togglePlayPause();
+    });
 
     // Handle the stop action on mediaSession
-    navigator.mediaSession.setActionHandler("stop", stopMusic);
+    navigator.mediaSession.setActionHandler('stop', function() {
+        stopMusic();
+    });
 
     // Handle the seekbackward action on mediaSession
-    navigator.mediaSession.setActionHandler("seekbackward", seekBackward);
+    navigator.mediaSession.setActionHandler('seekbackward', function() {
+        seekBackward();
+    });
 
     // Handle the seekforward action on mediaSession
-    navigator.mediaSession.setActionHandler("seekforward", seekForward);
+    navigator.mediaSession.setActionHandler('seekforward', function() {
+        seekForward();
+    });
 
     // Handle the seekto action on mediaSession
-    navigator.mediaSession.setActionHandler("seekto", seekTo);
+    navigator.mediaSession.setActionHandler('seekto', function() {
+        seekTo();
+    });
 
     // Handle the previoustrack action on mediaSession
-    navigator.mediaSession.setActionHandler("previoustrack", playLastMusic);
+    navigator.mediaSession.setActionHandler('previoustrack', function() { 
+        playLastMusic();
+    });
 
     // Handle the nexttrack action on mediaSession
-    navigator.mediaSession.setActionHandler("nexttrack", playNextMusic);
+    navigator.mediaSession.setActionHandler('nexttrack', function() {
+        playNextMusic();
+    });
 }
