6a5c0017b68023e908afb11e0cb8ca633654c95c
Pierre L'Hopital <lhopital.pierre@gmail.com>
2021-01-26 15:21:59 +0100

    fix media session handler

--- js/media-session.js
+++ js/media-session.js
@@ -1,26 +1,26 @@
 if ('mediaSession' in navigator) {
     
     // Handle the play action on mediaSession
-    navigator.mediaSession.setActionHandler('play', togglePlayPause, false);
+    navigator.mediaSession.setActionHandler("play", togglePlayPause);
 
     // Handle the pause action on mediaSession
-    navigator.mediaSession.setActionHandler('pause', togglePlayPause, false);
+    navigator.mediaSession.setActionHandler("pause", togglePlayPause);
 
     // Handle the stop action on mediaSession
-    navigator.mediaSession.setActionHandler('stop', stopMusic, false);
+    navigator.mediaSession.setActionHandler("stop", stopMusic);
 
     // Handle the seekbackward action on mediaSession
-    navigator.mediaSession.setActionHandler('seekbackward', seekBackward, false);
+    navigator.mediaSession.setActionHandler("seekbackward", seekBackward);
 
     // Handle the seekforward action on mediaSession
-    navigator.mediaSession.setActionHandler('seekforward', seekForward, false);
+    navigator.mediaSession.setActionHandler("seekforward", seekForward);
 
     // Handle the seekto action on mediaSession
-    navigator.mediaSession.setActionHandler('seekto', seekTo, false);
+    navigator.mediaSession.setActionHandler("seekto", seekTo);
 
     // Handle the previoustrack action on mediaSession
-    navigator.mediaSession.setActionHandler('previoustrack', playLastMusic, false);
+    navigator.mediaSession.setActionHandler("previoustrack", playLastMusic);
 
     // Handle the nexttrack action on mediaSession
-    navigator.mediaSession.setActionHandler('nexttrack', playNextMusic, false);
+    navigator.mediaSession.setActionHandler("nexttrack", playNextMusic);
 }
