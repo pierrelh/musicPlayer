bd8dc393c30397ba4b5912e006227e907675549d
Pierre L'Hopital <lhopital.pierre@gmail.com>
2021-10-09 18:00:25 +0200

    Rework JS

--- assets/reader.php
+++ assets/reader.php
@@ -7,18 +7,16 @@
 <section id="AudioPlayer" class="left">
 	<p id="MusicName"></p>
 	<section id="Controls">
-		<button>
-			<img id="Loop" class="reader-buttons loop" src="<?php echo $link ?>/img/loop.png" alt="loop">
-		</button>
-		<img id="Previous" class='reader-buttons' src="<?php echo $link ?>/img/back.png" alt="">
-		<img id="PlayPause" class="reader-buttons play" src="<?php echo $link ?>/img/pause.png" alt="play">
-		<img id="Next" class='reader-buttons' src="<?php echo $link ?>/img/skip.png" alt="">
+		<button id="Loop"><img class="reader-buttons loop" src="<?php echo $link ?>/img/loop.png" alt="Loop"></button>
+		<button id="Previous"><img class='reader-buttons' src="<?php echo $link ?>/img/back.png" alt="Previous"></button>
+		<button id="PlayPause"><img class="reader-buttons play" src="<?php echo $link ?>/img/pause.png" alt="Play"></button>
+		<button id="Next"><img class='reader-buttons' src="<?php echo $link ?>/img/skip.png" alt="Next"></button>
 		<span id="Start" class="time current-time">00:00</span>
 		<input type="range" min="0" max="100" value="0" id="ProgressBar" name="" value="">
 		<span id="Time" class="time total-time">00:00</span>
-		<img id="Random" class="reader-buttons" src="<?php echo $link ?>/img/no-random.png" alt="random">
-		<img id="Mute" class="reader-buttons mute" src="<?php echo $link ?>/img/audio-on.png" alt="">
+		<button id="Random"><img class="reader-buttons" src="<?php echo $link ?>/img/no-random.png" alt="Random"></button>
+		<button id="Mute"><img class="reader-buttons mute" src="<?php echo $link ?>/img/audio-on.png" alt="Mute"></button>
 		<input type="range" min="0" max="100" value="100" id="Volume" name="" value="">
-		<img id="PlaylistBtn" class="reader-buttons playlist-button" src="<?php echo $link ?>/img/playlist.png" alt="playlist">
+		<button id="PlaylistBtn"><img class="reader-buttons playlist-button" src="<?php echo $link ?>/img/playlist.png" alt="playlist"></button>
 	</section>
 </section>
