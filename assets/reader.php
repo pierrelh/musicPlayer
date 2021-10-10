5e1847e7e7016b07b1d09329573d59c13c498f61
Pierre L'Hopital <lhopital.pierre@gmail.com>
2021-10-10 12:20:24 +0200

    Rework JS

--- assets/reader.php
+++ assets/reader.php
@@ -7,17 +7,17 @@
 <section id="AudioPlayer">
 	<p id="MusicName"></p>
 	<section id="Controls">
-		<button	id="Loop"><img id="LoopIMG" class="reader-buttons loop" src="<?php echo $link ?>/img/loop.png" alt="Loop"></button>
-		<button	id="Previous"><img id="PreviousIMG" class='reader-buttons' src="<?php echo $link ?>/img/back.png" alt="Previous"></button>
-		<button	id="PlayPause"><img id="PlayPauseIMG" class="reader-buttons play" src="<?php echo $link ?>/img/pause.png" alt="Play"></button>
-		<button	id="Next"><img id="NextIMG" class='reader-buttons' src="<?php echo $link ?>/img/skip.png" alt="Next"></button>
-		<span	id="CurrentTime" class="time current-time">00:00</span>
+		<button	id="Loop" class="reader-buttons"><img id="LoopIMG" src="<?php echo $link ?>/img/loop.png" alt="Loop"></button>
+		<button	id="Previous" class="reader-buttons"><img id="PreviousIMG" src="<?php echo $link ?>/img/back.png" alt="Previous"></button>
+		<button	id="PlayPause" class="reader-buttons"><img id="PlayPauseIMG" src="<?php echo $link ?>/img/pause.png" alt="Play"></button>
+		<button	id="Next" class="reader-buttons"><img id="NextIMG" src="<?php echo $link ?>/img/skip.png" alt="Next"></button>
+		<span	id="CurrentTime" class="current-time">00:00</span>
 		<input	id="ProgressBar" type="range" min="0" max="100" value="0">
 		<span	id="EndTime" class="time total-time">00:00</span>
-		<button	id="Random"><img id="RandomIMG" class="reader-buttons" src="<?php echo $link ?>/img/no-random.png" alt="Random"></button>
-		<button	id="Mute"><img id="MuteIMG" class="reader-buttons mute" src="<?php echo $link ?>/img/audio-on.png" alt="Mute"></button>
+		<button	id="Random" class="reader-buttons"><img id="RandomIMG" src="<?php echo $link ?>/img/no-random.png" alt="Random"></button>
+		<button	id="Mute" class="reader-buttons"><img id="MuteIMG" src="<?php echo $link ?>/img/audio-on.png" alt="Mute"></button>
 		<input	id="Volume" type="range" min="0" max="100" value="100">
-		<button	id="PlaylistBtn"><img class="reader-buttons playlist-button" src="<?php echo $link ?>/img/playlist.png" alt="playlist"></button>
+		<button	id="PlaylistBtn" class="reader-buttons"><img src="<?php echo $link ?>/img/playlist.png" alt="playlist"></button>
 	</section>
 </section>
 <script type="text/javascript" src="<?php echo $link ?>/scripts/reader.js?cachev=<?php echo $cacheVersion ?>"></script>
