8eeba8fcf98db38943eb347488e20f5e073800d0
Pierre L'Hopital <lhopital.pierre@gmail.com>
2021-10-24 16:58:49 +0200

    Rework covers uploads

--- functions/files/editFile.php
+++ functions/files/editFile.php
@@ -9,14 +9,13 @@
 
 		// Uploading on overwriting the new cover
 		include_once($_SERVER['DOCUMENT_ROOT']."/functions/cloudinary/cloudinaryUpload.php");
-		error_log(print_r($_POST['covers'], true));
-		$covers = json_encode($_POST['covers']);
-		error_log(print_r($covers, true));
-		// foreach ($covers as $directory => $path) {
-		// 	$size = str_replace("x", '', $directory);
-		// 	$key = 'file_cover_' . $size;
-		// 	$_POST[$key] = uploadCover($files["tmp_name"], $path, true, $directory, intval($size));
-		// }
+		foreach ($_POST['covers'] as $directory => $path) {
+			error_log($directory);
+			error_log($path);
+			$size = str_replace("x", '', $directory);
+			$key = 'file_cover_' . $size;
+			$_POST[$key] = uploadCover($files["tmp_name"], $path, true, $directory, intval($size));
+		}
 	}
 
 	unset($_POST['covers'],$_POST['file_image']);
