5973a36e2f1cb3f4b1dfbae58bd67986f289dfc1
Pierre L'Hopital <lhopital.pierre@gmail.com>
2021-11-02 22:15:33 +0100

    Rework PHP

--- index.php
+++ index.php
@@ -3,10 +3,11 @@
 	$rootURL = 'https://' . $_SERVER['HTTP_HOST'];
 	$cacheVersion = "20211023-2";
 	require_once($_SERVER['DOCUMENT_ROOT'] . '/class/User.php');
+	$user = new User();
 
 	switch (isset($_COOKIE['SESSION_ID'])) {
 		case true:
-			$GLOBALS['User']->CheckIdentification();
+			$user->CheckIdentification();
 			require_once($_SERVER['DOCUMENT_ROOT'].'/class/Files.php');
 			require_once($_SERVER['DOCUMENT_ROOT'].'/class/Storage.php');
 			require_once($_SERVER['DOCUMENT_ROOT'].'/class/Playlist.php');
